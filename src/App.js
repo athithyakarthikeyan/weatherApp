4faad3ab0914f56a733fef9a2571965584faa679
Athithya <athithyakarthikeyan@gmail.com>
2024-06-11 15:26:54 +0530

    Added Framer Motion visual effects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import axios from "axios";
 import { Button, Input } from "antd";
 import "./App.css";
 import Card from "./Card";
+import { motion } from "framer-motion";
 
 function App() {
   const API_KEY = 'f50c3f2125830d5edd9c58a0955fa928';
@@ -26,7 +27,10 @@ function App() {
 
   return (
     <div className="app">
+      <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay: 0.3,duration:1}}>
       <h1 className="title">Weather App.</h1>
+      </motion.div>
+      <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay: 2, duration:1}}>
       <div className="search">
         <div className="search-bar">
           <Input
@@ -37,15 +41,24 @@ function App() {
         </div>
         <Button onClick={getWeatherData}>Search</Button>
       </div>
+      </motion.div>
       <div className="weather-cards">
         {weatherData.map((data, index) => (
-          <Card
-            cityName={data.name}
-            cityState={data.weather[0].description}
-            cityTemp={((data.main.temp - 273.15)).toFixed(2) + "°C"}
-          />
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: index * 0.1 }}
+          >
+            <Card
+              cityName={data.name}
+              cityState={data.weather[0].description}
+              cityTemp={(data.main.temp - 273.15).toFixed(2) + "°C"}
+            />
+          </motion.div>
         ))}
       </div>
+      
     </div>
   );
 }

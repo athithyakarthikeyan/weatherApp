ef61fb3445ee4bdc3c9458e70f019a1f22735787
Athithya <athithyakarthikeyan@gmail.com>
2025-02-09 23:47:34 +0530

    Changes to code

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [weather, setWeather] =  useState(0);
+  const [city, setCity] = useState("");
+  const [place, setPlace] = useState("");
+  const [show, setShow] = useState(false);
+  const API_KEY = "f50c3f2125830d5edd9c58a0955fa928";
+  
+
+
+  const detectInput = (e) =>{
+    setCity(e.target.value);
+  }
+  
+  const fetchWeather = () =>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`).then((res)=>{
+      setWeather(res.data.main.temp);
+      setPlace(res.data.name);
+    }
+  )
+  }
+
+  useEffect(()=>{
+    if (weather!=0) setShow(true);
+  },[weather])
+
+  return(
+    <div>
+      <input placeholder='Enter your city' onChange={detectInput}></input>
+      <button onClick={fetchWeather}>Find the weather!</button>
+      {show && <h1>The weather at {place} right now is {weather}°C.</h1>}
+    </div>
+  )
+}
+
+
+export default App
